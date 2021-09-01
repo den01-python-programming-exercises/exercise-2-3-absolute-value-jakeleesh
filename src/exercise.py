e7b73cb2feb047e6e8961f9fba2b45652e9cc6b4
Jake Lee <jake.lee@fdmgroup.com>
2021-09-01 13:40:19 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    num = int(input())
+    if (num < 0):
+        print(num * -1)
+    else:
+        print(num)
 
 if __name__ == '__main__':
     main()
